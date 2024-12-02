bd11e1a410ce37c8e75173a6bcaecffcc4a6cd77
Euan Kemp <euank@euank.com>
2024-12-02 22:45:04 +0900

    day02: part2

--- day02/default.nix
+++ day02/default.nix
@@ -7,53 +7,40 @@ let
   parseInput = input:
     map (line: map toInt (splitString " " line)) (splitString "\n" input);
 
+  isSafe = line:
+    if (length line) <= 2 then
+      true
+    else
+      let
+        h = head line;
+        n = head (tail line);
+        nn = head (tail (tail line));
+        # Increasing or decreasing
+      in ((n > h && nn > n) || ((n < h) && nn < n))
+      # Diff
+      && ((abs (n - h)) <= 3) && ((abs (nn - n)) <= 3)
+      # At least 1 diff
+      && n != h && n != nn
+      # And holds for the rest
+      && isSafe (tail line);
+
   part1Answer = input:
-    let
-      p = parseInput input;
-      isSafe = line:
-        if (length line) <= 2 then
-          true
-        else
-          let
-            h = head line;
-            n = head (tail line);
-            nn = head (tail (tail line));
-            # Increasing or decreasing
-          in ((n > h && nn > n) || ((n < h) && nn < n))
-          # Diff
-          && ((abs (n - h)) <= 3) && ((abs (nn - n)) <= 3)
-          # At least 1 diff
-          && n != h && n != nn
-          # And holds for the rest
-          && isSafe (tail line);
+    let p = parseInput input;
     in foldl' (acc: line: acc + (if isSafe line then 1 else 0)) 0 p;
 
   part2Answer = input:
     let
       p = parseInput input;
-      isSafe = line: hasSkipped:
-        if (length line) <= 2 then
-          true
-        else
-          let
-            isSafeNoSkip = let
-              h = head line;
-              n = head (tail line);
-              nn = head (tail (tail line));
-              # Increasing or decreasing
-            in ((n > h && nn > n) || ((n < h) && nn < n))
-            # Diff
-            && ((abs (n - h)) <= 3) && ((abs (nn - n)) <= 3)
-            # At least 1 diff
-            && n != h && n != nn
-            # And holds for the rest
-            && isSafe (tail line) hasSkipped;
-          in isSafeNoSkip || (!hasSkipped && (
-            # skip first
-            (isSafe (tail line) true)
-            # skip second
-            || (isSafe ((take 1 line) ++ (drop 2 line)) true)));
-    in foldl' (acc: line: acc + (if (isSafe line false) then 1 else 0)) 0 p;
+      # This is brute-forceable, so just do that lol
+      # I know there's more clever answers
+      anySafe = line:
+        let
+          perms = builtins.genList
+            (nozoku: (take nozoku line) ++ (drop (nozoku + 1) line))
+            (length line);
+        in (isSafe line) || any (isSafe) perms;
+
+    in foldl' (acc: line: acc + (if (anySafe line) then 1 else 0)) 0 p;
 
 in {
   part1 = part1Answer input;
