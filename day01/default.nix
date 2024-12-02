7c043bbe1b7368ba99320c951be0d55c1adef85d
Euan Kemp <euank@euank.com>
2024-12-02 21:26:45 +0900

    day01: part2

--- day01/default.nix
+++ day01/default.nix
@@ -22,4 +22,20 @@ let
     in foldl' (acc: p: acc + (abs (p.fst - p.snd))) 0
     (lists.zipLists sorted1 sorted2);
 
-in { part1 = part1Answer input; }
+  part2Answer = input:
+    let
+      p = parseInput input;
+      grouped1 = lists.groupBy toString p.col1;
+      grouped2 = lists.groupBy toString p.col2;
+
+      mathAttr = k: v:
+        if grouped2 ? "${k}" then
+          (toInt k) * (length v) * (length grouped2.${k})
+        else
+          0;
+    in foldl' builtins.add 0 (attrsets.mapAttrsToList mathAttr grouped1);
+
+in {
+  part1 = part1Answer input;
+  part2 = part2Answer input;
+}
