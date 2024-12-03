488ef57f31737a10c000c1d6a94afa4bdf6105f8
Euan Kemp <euank@euank.com>
2024-12-03 14:23:46 +0900

    day03: part2

--- day03/default.nix
+++ day03/default.nix
@@ -15,4 +15,31 @@ let
           foldl' (acc: p: acc * p) 1 (map toInt matches);
     in foldl' builtins.add 0 (map parsePiece pieces);
 
-in { part1 = part1Answer input; }
+  part2Answer = input:
+    let
+      matches =
+        builtins.split "mul\\(([0-9]+,[0-9]+)\\)|(do\\(\\))|(don't\\(\\))"
+        input;
+      instrs =
+        remove null (flatten (map (e: if isList e then e else null) matches));
+      step = state: instr:
+        if instr == "do()" then
+          state // { disabled = false; }
+        else if instr == "don't()" then
+          state // { disabled = true; }
+        else if state.disabled then
+          state
+        else
+          let parts = splitString "," instr;
+          in state // {
+            val = state.val + (foldl' builtins.mul 1 (map toInt parts));
+          };
+    in (foldl' step {
+      val = 0;
+      disabled = false;
+    } instrs).val;
+
+in {
+  part1 = part1Answer input;
+  part2 = part2Answer input;
+}
