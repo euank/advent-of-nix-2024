c17d13c647b26af59d6a4287694daba404e15597
Euan Kemp <euank@euank.com>
2024-12-04 14:43:14 +0900

    day04: part1

---
+++ day04/default.nix
@@ -0,0 +1,41 @@
+{ nixpkgs, lib, ... }:
+with nixpkgs.lib;
+with lib;
+let
+  input = fileContents ./input;
+
+  parseInput = input: map stringToCharacters (splitString "\n" input);
+
+  part1Answer = input:
+    let
+      grid = parseInput input;
+      countXmasFrom = x: y:
+        let
+          el = arr2.get grid x y;
+          o3 = builtins.genList (i: i + 1) 3;
+        in if el != "X" then
+          0
+        else
+        # Check each direction
+          let
+            left = map (offset: arr2.getDef grid (x - offset) y "") o3;
+            right = map (offset: arr2.getDef grid (x + offset) y "") o3;
+            up = map (offset: arr2.getDef grid x (y - offset) "") o3;
+            down = map (offset: arr2.getDef grid x (y + offset) "") o3;
+            upleft =
+              map (offset: arr2.getDef grid (x - offset) (y - offset) "") o3;
+            upright =
+              map (offset: arr2.getDef grid (x + offset) (y - offset) "") o3;
+            downleft =
+              map (offset: arr2.getDef grid (x - offset) (y + offset) "") o3;
+            downright =
+              map (offset: arr2.getDef grid (x + offset) (y + offset) "") o3;
+          in foldl' (acc: dir: acc + (if dir == [ "M" "A" "S" ] then 1 else 0))
+          0 [ left right up down upleft upright downleft downright ];
+    in foldl' builtins.add 0
+    (flatten (arr2.imap (x: y: _: countXmasFrom x y) grid));
+
+in {
+  part1 = part1Answer input;
+  # part2 = part2Answer input;
+}
--- lib.nix
+++ lib.nix
@@ -7,5 +7,21 @@ let
       (builtins.filter builtins.isList (builtins.split "([^ ]+)" s));
 
     abs = num: if num < 0 then (-1) * num else num;
+
+    arr2 = rec {
+      width = arr: if (length arr) == 0 then 0 else length (elemAt arr 0);
+      height = length;
+
+      get = arr: x: y: elemAt (elemAt arr y) x;
+
+      getDef = arr: x: y: def:
+        if x < 0 || y < 0 then def
+        else if x >= (width arr) || y >= (height arr) then def
+        else elemAt (elemAt arr y) x;
+
+      imap = f: arr:
+        genList (y: genList (x: f x y (get arr x y)) (length (head arr)))
+        (length arr);
+    };
   };
 in lib
