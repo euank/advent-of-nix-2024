e8e31382d8d29efc78df8c59d6ec16a667c4f31c
Euan Kemp <euank@euank.com>
2024-12-09 19:07:12 +0900

    day08: part2

--- day08/default.nix
+++ day08/default.nix
@@ -36,16 +36,66 @@ let
       }
     );
 
+  valid =
+    an: w: h:
+    an.x >= 0 && an.y >= 0 && an.x < w && an.y < h;
+
   part1Answer =
     input:
     let
       p = parseInput input;
       ans = flatten (map findAntinodes (attrValues p.locs));
     in
-    length (unique (filter ({ x, y }: x >= 0 && y >= 0 && x < p.width && y < p.height) ans));
+    length (unique (filter (an: valid an p.width p.height) ans));
+
+  antiNodesL =
+    l: r: w: h:
+    let
+      nl = {
+        x = l.x - (r.x - l.x);
+        y = l.y - (r.y - l.y);
+      };
+    in
+    if valid nl w h then [ nl ] ++ (antiNodesL nl l w h) else [ ];
+
+  antiNodesR =
+    l: r: w: h:
+    let
+      nr = {
+        x = r.x + (r.x - l.x);
+        y = r.y + (r.y - l.y);
+      };
+    in
+    if valid nr w h then [ nr ] ++ (antiNodesR r nr w h) else [ ];
+
+  antiNodes2 =
+    l: r: w: h:
+    [
+      { inherit (l) x y; }
+      { inherit (r) x y; }
+    ]
+    ++ (antiNodesL r l w h)
+    ++ (antiNodesR l r w h);
+
+  findAntinodes2 =
+    width: height: charCoords:
+    flatten (
+      mapCartesianProduct ({ l, r }: if l == r then [ ] else antiNodes2 l r width height) {
+        l = charCoords;
+        r = charCoords;
+      }
+    );
+
+  part2Answer =
+    input:
+    let
+      p = parseInput input;
+      ans = flatten (map (v: findAntinodes2 p.width p.height v) (attrValues p.locs));
+    in
+    length (unique ans);
 
 in
 {
   part1 = part1Answer input;
-  # part2 = part2Answer input;
+  part2 = part2Answer input;
 }
