9eb4aafb4cde48168dd5106fd7a5bef5e8842cae
Euan Kemp <euank@euank.com>
2024-12-29 23:32:49 +0900

    day17: part2

--- day17/default.nix
+++ day17/default.nix
@@ -32,7 +32,7 @@ let
         c
         out
         ;
-      instr = debug.traceValSeq (elemAt state.prog state.ip);
+      instr = elemAt state.prog state.ip;
       operand = elemAt state.prog (state.ip + 1);
       ip = state.ip + 2;
       combo =
@@ -119,7 +119,7 @@ let
           state
           // {
             inherit ip;
-            c = a / (pow 2 (debug.traceValSeq (combo operand)));
+            c = a / (pow 2 (combo operand));
           }
         )
       )
@@ -132,8 +132,73 @@ let
       p = parseInput input;
     in
     concatMapStringsSep "," toString (step (p // { out = [ ]; }));
+
+  # Let's manually run a couple rounds of our input:
+  # Register A: 47719761
+  # Register B: 0
+  # Register C: 0
+  # Program: 2,4,1,5,7,5,0,3,4,1,1,6,5,5,3,0
+  #
+  # while a > 0 {
+  #   b = a % 8
+  #   b = b ^ 5
+  #   c = a / (2 ** b)
+  #   a = a / 8
+  #   b = b ^ c
+  #   b = b ^ 6
+  #   out(b % 8)
+  # }
+  #
+  # Simplifying, and shuffling some stuff to octal for clarity, we get:
+  #
+  # for (a = ?; a > 0; a = a << 3) {
+  # while a > 0 {
+  #   b = (a % 0o1000) ^ 0o0101
+  #   c = a / (2 ** b)
+  #   out(((b ^ c) ^ (0o00110)) % 8)
+  # }
+  #
+  # So basically, each loop we take a's lowest 3 bits (the mod), and xor them by a constant.
+  # We then output a number based purely on a + those bits, and then take those bits off a.
+  #
+  # That means that the last digit output is based on a's last 3 bits only, and
+  # as we step left, we can brute-force 3 bits at a time to figure things out I think.
+  bruteForce =
+    idx: state: a:
+    let
+      out = step (
+        state
+        // {
+          out = [ ];
+          inherit a;
+        }
+      );
+    in
+    # we multiplied by 8 when we found the last digit, knock that back off
+    if idx == (length state.prog) then
+      a / 8
+    # multiply by 8 to start looking for the next 3 bits
+    else if out == (sublist ((length state.prog) - idx - 1) (length state.prog) state.prog) then
+      bruteForce (idx + 1) state (a * 8)
+    else
+      bruteForce idx state (a + 1);
+
+  part2Answer =
+    input:
+    let
+      state = parseInput input;
+      a = bruteForce 0 state 0;
+      out = step (
+        state
+        // {
+          out = [ ];
+          inherit a;
+        }
+      );
+    in
+    if out != state.prog then throw "${builtins.toJSON out} != ${builtins.toJSON state.prog}" else a;
 in
 {
   part1 = part1Answer input;
-  # part2 = part2Answer input;
+  part2 = part2Answer input;
 }
