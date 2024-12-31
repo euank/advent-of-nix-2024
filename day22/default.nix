c9a990236a0a9371b5c727f99827ccbb606edc33
Euan Kemp <euank@euank.com>
2024-12-31 21:30:49 +0900

    day22: part2

--- day22/default.nix
+++ day22/default.nix
@@ -25,8 +25,58 @@ let
     in
     foldl' builtins.add 0 (map (n: foldl' (acc: el: step acc) n (builtins.genList trivial.id 2000)) p);
 
+  seqVals =
+    seq: res:
+    if (length seq) < 5 then
+      res
+    else
+      let
+        seq' = concatStringsSep "," (
+          map toString (builtins.genList (i: (elemAt seq (i + 1)) - (elemAt seq i)) 4)
+        );
+      in
+      if res ? "${seq'}" then
+        seqVals (tail seq) res
+      else
+        (seqVals (tail seq) (forceShallow (res // { "${seq'}" = elemAt seq 4; })));
+
+  findBest =
+    seqs:
+    let
+      maps = map (s: force (seqVals s { })) seqs;
+      merged = zipAttrsWith (name: values: foldl' builtins.add 0 values) maps;
+    in
+    foldl' (m: el: if el.value > m.value then el else m) { name = ""; value = 0; } (attrsToList merged);
+
+  part2Answer =
+    input:
+    let
+      p = parseInput input;
+      seqs = map (
+        n:
+        (foldl'
+          (
+            acc: _:
+            let
+              n' = step acc.n;
+            in
+            {
+              n = n';
+              seq = acc.seq ++ [ (mod n' 10) ];
+            }
+          )
+          {
+            seq = [ (mod n 10) ];
+            n = n;
+          }
+          (builtins.genList trivial.id 2000)
+        ).seq
+      ) p;
+    in
+    findBest seqs;
+
 in
 {
   part1 = part1Answer input;
-  # part2 = part2Answer input;
+  part2 = part2Answer input;
 }
